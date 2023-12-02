24129bef1bee2e77cd4a347e1a66705344296580
work.ducmanh0323 <work.ducmanh0323@gmail.com>
2023-12-02 12:27:10 +0700

    Remove unused variables in general store

--- stores/general.ts
+++ stores/general.ts
@@ -1,19 +1,9 @@
 export const useGeneralStore = defineStore('general', {
   state: () => ({
-    isAsideVisible: true,
     isFullPageLoading: false,
   }),
   getters: {},
   actions: {
-    hideAside() {
-      this.isAsideVisible = false;
-    },
-    showAside() {
-      this.isAsideVisible = true;
-    },
-    toggleAsideVisible() {
-      this.isAsideVisible = !this.isAsideVisible;
-    },
     showFullPageLoading() {
       this.isFullPageLoading = true;
     },
